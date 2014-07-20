78d88dc9a32c8c9fddf6129e8476d792f179cdc2
hzlbrc <bagburcu@hotmail.com>
2014-07-21 02:07:25 +0300

    msj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Merhaba::Application.config.secret_key_base = '06a7f7292c99b5ff419adc0a0605efe82c0f1b24941314949c4d32bbe9d1f307b3fd5a7020e6a1a753d4ae53960afddcb5bfd7c374f60ef6e3fc42ae25bf2481'
